f0313b929cd8746929a8d2ca2fb74bcd7c07db0a
Satomi Richardson <strichardson@ucdavis.edu>
2023-02-12 20:13:59 -0800

    madlibs edited

--- studio1/script.js
+++ studio1/script.js
@@ -1,36 +1,34 @@
-// IIFE
-// (function(){
-// 'use strict';
-//  console.log('reading.js');
-// }())
-
 (function(){
   'use strict';
   console.log('reading.js');
 
   const myform = document.getElementById('myform');
   const madlib = document.querySelector('#madlib');
+  let textSize = document.querySelector('#frame');
+  
+
 
   myform.addEventListener('submit', function(event){
       event.preventDefault();
+      document.getElementsByClassName('homeBase')[0].className = 'hidden';
       const fname = document.querySelector('#fname').value;
       const verb = document.querySelector('#verb').value;
-      const noun = document.querySelector('#noun').value;
+      const noun1 = document.querySelector('#noun1').value;
       const noun2 = document.querySelector('#noun2').value;
-      const adj = document.querySelector('#adj').value;
+      const adj1 = document.querySelector('#adj1').value;
       const adj2 = document.querySelector('#adj2').value;
       const birthday = document.querySelector('#birthday').value;
       
       let myText;
       if(fname== ''){
           myText = `please provide a name`;
-          document.querySelector('fname').focus();
+          document.querySelector('#fname').focus();
       } else if(noun1==''){
           myText = `please provide a noun`;
           document.querySelector('#noun1').focus();
-     } else if(adj==''){
+     } else if(adj1==''){
           myText = `please provide a adjective`;
-          document.querySelector('#adj').focus();
+          document.querySelector('#adj1').focus();
       } else if(adj2==''){
           myText = `please provide a adjective`;
           document.querySelector('#adj2').focus();
@@ -38,18 +36,12 @@
           myText = `please provide a verb`;
           document.querySelector('#verb').focus();
       } else {
-          myText= `${fname}, your sign is${birthday}.
-          You are very ${adj} at school. When you feel ${adj2} you do anything to ${verb} for ${noun}.
-          All qualities make ${birthday} a great ${noun2}.`;   
+          myText= `${fname}, your sign is ${birthday}.
+          You are very ${adj1} at school. When you feel ${adj2} you do anything to ${verb} for ${noun1}.
+          All qualities make ${birthday} a great ${noun2}.`; 
       }
-      
+
       madlib.innerHTML = myText;
-      // document.querySelector('#fname').value ='';
-      // document.querySelector('#verb').value ='';
-      // document.querySelector('#noun1').value ='';
-      // document.querySelector('#adj').value ='';
-      // document.querySelector('#adj2').value ='';
-      // document.querySelector('#noun2').value ='';
-      // document.querySelector('#birthday').value ='';
+      document.getElementById('overlay').className = 'showing';
   })
   }())
